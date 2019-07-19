4ea992b388f3862a3e75640abb28009fd080fc3a
lcesquivel10 <lc.esquivel10@uniandes.edu.co>
2019-07-19 07:58:02 -0500

    intento de pdf

--- hw2.mk
+++ hw2.mk
@@ -1,4 +1,10 @@
-prueba.tex: plots.png GenerarDatos ## prueba.tex engaño
+
+##GeneradPdf: GenerarDatos resultados.tex
+##	pdflatex resultados.tex
+##	find . -type f -not -name '*py' -not -name '*tex' -not -name '*mk' -not -name ##'*png' -not -name 'resultados.pdf' -print0 | xargs -0 rm --	
+
+
+prueba.tex: plots.png GenerarDatos ## prueba engaño.. cabeza engaño
 
 plots.png: data_euler.dat data_leap.dat data_runge.dat
 	python Plots_hw2.py
