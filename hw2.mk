a02f9b842426be49e2601179c10cdd1d50015dfb
lcesquivel10 <lc.esquivel10@uniandes.edu.co>
2019-07-18 23:34:32 -0500

    primer makefile

--- hw2.mk
+++ hw2.mk
@@ -1,2 +1,15 @@
-.cpp depende de .aout
-grafias dependen de datos
+prueba.tex: plots.png GenerarDatos
+
+plots.png: data_euler.dat data_leap.dat data_runge.dat
+	python Plots_hw2L.py
+
+%.dat : a.out
+	./a.out
+
+a.out: ODEsL.cpp
+	g++ -o a.out ODEsL.cpp
+       
+GenerarDatos: fourier.py
+	python3 fourier.py
+    
+
