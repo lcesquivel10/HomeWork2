dd3d956e3c88127499ad8f1bc630c6a17131280f
lcesquivel10 <lc.esquivel10@uniandes.edu.co>
2019-07-19 01:11:12 -0500

    posible final

--- hw2.mk
+++ hw2.mk
@@ -1,13 +1,13 @@
 prueba.tex: plots.png GenerarDatos
 
 plots.png: data_euler.dat data_leap.dat data_runge.dat
-	python Plots_hw2L.py
+	python Plots_hw2.py
 
 %.dat : a.out
 	./a.out
 
-a.out: ODEsL.cpp
-	g++ -o a.out ODEsL.cpp
+a.out: ODEs.cpp
+	g++ -o a.out ODEs.cpp
        
 GenerarDatos: fourier.py
 	python3 fourier.py
